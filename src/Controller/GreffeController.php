7fed616a4cb4bf85dcb842800351328ee6c63c4d
MacBoook03 <macboook03@MacBook-Pro-de-MacBook04.local>
2025-12-02 09:34:36 +0100

    controller + bundel

---
+++ src/Controller/GreffeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class GreffeController extends AbstractController
+{
+    #[Route('/greffe', name: 'app_greffe')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/GreffeController.php',
+        ]);
+    }
+}
