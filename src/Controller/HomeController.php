2736836f7623280f24ff6b98d18a97a3f8cb102e
MacBoook03 <macboook03@MacBook-Pro-de-MacBook04.local>
2025-12-11 13:03:11 +0100

    pb_route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,15 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route(path: '/', name: 'app_home')]
+        #[Route(path: '/', name: 'app_home')]
     public function index(): Response
     {
-        // Si l'utilisateur est connecté, afficher le dashboard
         if ($this->getUser()) {
             return $this->render('home/index.html.twig');
         }
 
-        // Sinon, rediriger vers la page de login
         return $this->redirectToRoute('app_login');
-    }
+}
+
+    
 }
